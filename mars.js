e618152203164c2884423651765afb423457b40d
Tyler Olsen <tylerkolsen@gmail.com>
2024-07-30 14:51:13 -0600

    Finished lab. Basic features are working

--- mars.js
+++ mars.js
@@ -1,40 +1,89 @@
-// // Alert portion for starting the game
-// alert("Starting your Mars Adventure!");
-// alert("Booting up...");
-// alert("All systems go!");
-// alert("Let's start!");
+// Alert portion for starting the game
+alert("Starting your Mars Adventure!");
+alert("Booting up...");
+alert("All systems go!");
+alert("Let's start!");
 
-// // First use of prompt, and template string addition
-// const username = prompt("Hi there. What's your name?");
-// alert(`Hi ${username} --- Welcome to The Mars Adventure Game.`)
-// alert("Yesterday, you received a call from your good friend at NASA")
-// alert("They need someone to go to Mars this weekend, and YOU'VE been chosen!!")
+// First use of prompt, and template string addition
+const username = prompt("Hi there. What's your name?");
+alert(`Hi ${username} --- Welcome to The Mars Adventure Game.`)
+alert("Yesterday, you received a call from your good friend at NASA")
+alert("They need someone to go to Mars this weekend, and YOU'VE been chosen!!")
 
-// // Conditional statement for excitement
-// let excited = prompt("Are you excited? Type Y or N")
-// excited = excited.toUpperCase();
-// if (excited === "Y") {
-//     alert("I knew you'd say that. It's so cool that you're going to Mars!")
-// } else if (excited === "N") {
-//     alert("Well, it's too late to back out now.")
-// }
+// Conditional statement for excitement
+let excited = prompt("Are you excited? Type Y or N")
+excited = excited.toUpperCase();
+if (excited === "Y") {
+    alert("I knew you'd say that. It's so cool that you're going to Mars!")
+} else if (excited === "N") {
+    alert("Well, it's too late to back out now.")
+}
 
-// // Data Conversion portion
-// alert("It's time to pack for your trip to Mars.");
-// let numSuitcases = prompt("How many suitcases do you plan to bring?");
-// numSuitcases = Number(numSuitcases);
-// if (numSuitcases > 2) {
-//     alert("That's way too many. You'll have to pack more lightly.")
-// } else {
-//     alert("Perfect. You'll certainly fit in the spaceship!")
-// }
+// Data Conversion portion
+alert("It's time to pack for your trip to Mars.");
+let numSuitcases = prompt("How many suitcases do you plan to bring?");
+numSuitcases = Number(numSuitcases);
+if (numSuitcases > 2) {
+    alert("That's way too many. You'll have to pack more lightly.")
+} else {
+    alert("Perfect. You'll certainly fit in the spaceship!")
+}
 
 // Companion Animal Portion
 alert("You're allowed to bring one companion animal with you.")
-const companionType = prompt("What kind of companion animal would you like to bring?")
-const companionName = prompt("What is your companion's name?")
+let companionType = prompt("What kind of companion animal would you like to bring?")
+let companionName = prompt("What is your companion's name?")
+let firstLetterName = companionName[0]
+firstLetterName = firstLetterName.toUpperCase()
+let otherLettersName = companionName.slice(1)
+otherLettersName = otherLettersName.toLowerCase()
+companionName = firstLetterName + otherLettersName
+
+let firstLetterType = companionType[0]
+firstLetterType = firstLetterType.toUpperCase()
+let otherLettersType = companionType.slice(1)
+otherLettersType = otherLettersType.toLowerCase()
+companionType = firstLetterType + otherLettersType
+
 alert(`Cool, so you're bringing ${companionName} the ${companionType}`)
 
-// Companion Animal capitalization portion
+// // Companion Animal capitalization portion (DON'T INCLUDE)
+// let companionName = "Remmy"
+// let firstLetter = companionName[0]
+// firstLetter = firstLetter.toUpperCase()
 
+// let otherLetters = companionName.slice(1)
+// otherLetters = otherLetters.toLowerCase()
 
+// companionName = firstLetter + otherLetters
+// console.log(companionName)
+
+// Spaceship Decor
+alert("NASA has an interior design team offering to outfit your space ship.")
+let decorChoice = prompt(`You have a couple of options for the interior decor of your ship. Your options are:
+A   Sleek, modern minimalism
+B   Retro/vintage space age
+C   Victorian-era steampunk
+Type in A, B or C
+`);
+decorChoice = decorChoice.toUpperCase()
+let decor;
+if (decorChoice === "A") {
+    decor = "modern minimalist"
+} else if (decorChoice === "B") {
+    decor = "Retro/vintage space age"
+} else if (decorChoice === "C") {
+    decor = "Victorian steampunk"
+}
+
+// Bringing it all together
+alert(`${username} and ${companionName}, surfing the celestial abyss, in a ${decor} spaceship.`)
+
+// Lift Off while loop
+
+let timer = 5;
+while (timer > 0) {
+    alert(`${timer}...`);
+    timer = timer - 1
+}
+alert("*** LIFTOFF ***");
