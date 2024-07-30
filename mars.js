55f8e7d470f7e335b41a56fcd167c72e26471c17
Tyler Olsen <tylerkolsen@gmail.com>
2024-07-30 14:14:36 -0600

    Addition up to Animal Companion, before capitalization

--- mars.js
+++ mars.js
@@ -1,19 +1,40 @@
-// Alert portion for starting the game
-alert("Starting your Mars Adventure!");
-alert("Booting up...");
-alert("All systems go!");
-alert("Let's start!");
+// // Alert portion for starting the game
+// alert("Starting your Mars Adventure!");
+// alert("Booting up...");
+// alert("All systems go!");
+// alert("Let's start!");
 
-// First use of prompt, and template string addition
-const username = prompt("Hi there. What's your name?");
-alert(`Hi ${username} --- Welcome to The Mars Adventure Game.`)
-alert("Yesterday, you received a call from your good friend at NASA")
-alert("They need someone to go to Mars this weekend, and YOU'VE been chosen!!")
+// // First use of prompt, and template string addition
+// const username = prompt("Hi there. What's your name?");
+// alert(`Hi ${username} --- Welcome to The Mars Adventure Game.`)
+// alert("Yesterday, you received a call from your good friend at NASA")
+// alert("They need someone to go to Mars this weekend, and YOU'VE been chosen!!")
 
-// Conditional statement for excitement
-const excited = prompt("Are you excited? Type Y or N")
-if (excited === "Y") {
-    alert("I knew you'd say that. It's so cool that you're going to Mars!")
-} else if (excited === "N") {
-    alert("Well, it's too late to back out now.")
-}
+// // Conditional statement for excitement
+// let excited = prompt("Are you excited? Type Y or N")
+// excited = excited.toUpperCase();
+// if (excited === "Y") {
+//     alert("I knew you'd say that. It's so cool that you're going to Mars!")
+// } else if (excited === "N") {
+//     alert("Well, it's too late to back out now.")
+// }
+
+// // Data Conversion portion
+// alert("It's time to pack for your trip to Mars.");
+// let numSuitcases = prompt("How many suitcases do you plan to bring?");
+// numSuitcases = Number(numSuitcases);
+// if (numSuitcases > 2) {
+//     alert("That's way too many. You'll have to pack more lightly.")
+// } else {
+//     alert("Perfect. You'll certainly fit in the spaceship!")
+// }
+
+// Companion Animal Portion
+alert("You're allowed to bring one companion animal with you.")
+const companionType = prompt("What kind of companion animal would you like to bring?")
+const companionName = prompt("What is your companion's name?")
+alert(`Cool, so you're bringing ${companionName} the ${companionType}`)
+
+// Companion Animal capitalization portion
+
+
